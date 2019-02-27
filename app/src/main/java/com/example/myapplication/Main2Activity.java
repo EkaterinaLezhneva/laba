20c1e78caa21c4d10feab7604c84eff2458d1b00
Ekaterina Lezhneva <you@example.com>
2019-02-27 18:44:45 +0300

    laba2-fragment

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,63 +1,65 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.app.FragmentManager;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
+
 
 
 public class Main2Activity extends AppCompatActivity {
 
-    Intent myIntent;
-    TextView nameUser;
-    private DrawerLayout mDrawerLayout;
+    Fragment fragment = new Weather();
+    Fragment fragment1 = new City();
+    Fragment fragment2 = new SavedCities();
+    Fragment fragment3 = new Author();
+
+    FragmentManager fragmentManager = getFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2_main);
 
-        myIntent = getIntent();
-        nameUser = findViewById(R.id.nameUser);
-        mDrawerLayout = findViewById(R.id.my_drawer_layout);
+        final android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+        FragmentTransaction replace = (FragmentTransaction) fragmentTransaction.replace(R.id.container, fragment);
+        replace.commit();
+        final DrawerLayout  mDrawerLayout = findViewById(R.id.my_drawer_layout);
+
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-                Fragment myfragment = new Fragment();
-                Class fragmentClass = null;
-                int idItem = menuItem.getItemId();
-                if (idItem == R.id.select_cities) {
-                    fragmentClass = City.class;
 
-                } else if (idItem == R.id.weather) {
-                    fragmentClass = Weather.class;
-                } else if (idItem == R.id.saved_cities) {
-                    fragmentClass = SavedCities.class;
-
-                } else if (idItem == R.id.author) {
-                    fragmentClass = Author.class;
-                }
-
-
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.container, new Fragment()).commit();
                 menuItem.setChecked(true);
                 mDrawerLayout.closeDrawers();
+
+                int idItem = menuItem.getItemId();
+                if (idItem == R.id.select_cities) {
+                    fragmentTransaction.replace(R.id.container, fragment1);
+                } else if (idItem == R.id.weather) {
+                    fragmentTransaction.replace(R.id.container, fragment);
+                } else if (idItem == R.id.saved_cities) {
+                    fragmentTransaction.replace(R.id.container, fragment2);
+
+                } else if (idItem == R.id.author) {
+                    fragmentTransaction.replace(R.id.container, fragment3);
+                }
+
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
                 return true;
             }
 
         });
 
-
-        if (myIntent != null) {
-            String name = myIntent.getStringExtra("nameUser");
-            nameUser.setText(name);
-        }
     }
 }
--- app/src/main/java/com/example/myapplication/Weather.java
+++ app/src/main/java/com/example/myapplication/Weather.java
@@ -1,12 +1,12 @@
 package com.example.myapplication;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 
 public class Weather extends Fragment {
     @Override
